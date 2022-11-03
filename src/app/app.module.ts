1b00eaf0a308813b9c513925fff82e6f60855d5e
Bartłomiej Witek <bwitek.kontakt@gmail.com>
2022-11-03 11:38:53 +0100

    Add header and map components and apply basic styling to header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule
